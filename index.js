c6521616624075295fd6e5c9775f35faff53bf7e
cowflipper <hoff3454@gmail.com>
2024-10-17 00:07:53 +0300

    4

--- index.js
+++ index.js
@@ -27,22 +27,16 @@ const input3 = document.querySelector(".input3");
 const input4 = document.querySelector(".input4");
 const input5 = document.querySelector(".input5");
 const stringnumber = document.querySelector(".stringNumber");
-const button = document.querySelector(".button");
 const buttonArr = document.querySelector(".btnArr");
 const buttonStart = document.querySelector(".btnStart");
 const countDown = document.querySelector(".countDown");
-let newString;
 let newArr = [];
 
-const inputHandler = () => {
-  newString = input1.value +" " + input2.value +" " + input3.value +" " +  input4.value +" " + input5.value;
-  stringnumber.innerHTML = newString;
-  console.log(newString);
-};
+
 
 const createArr = () => {
-  // newArr.push(input1.value, input2.value, input3.value, input4.value, input5.value,)
-  newArr = newString.split(" ");
+  newArr = [];
+  newArr.push(input1.value, input2.value, input3.value, input4.value, input5.value,)
   console.log(newArr);
   let isEqual = true;
   function arraysEqual(arr1, arr2) {
@@ -60,8 +54,6 @@ const createArr = () => {
     console.log("arrays are not correct");
   }
 };
-
-button.addEventListener("click", inputHandler);
 
 buttonArr.addEventListener("click", createArr);
 
